4ee84267e91582a4b5c7ea9c15bac0cb80613f4e
구기운 <9kiwoon@cclss.net>
2026-04-01 14:18:20 +0900

    배포 방식 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  base: './',
+  base: '/dark-forest-star/',
   resolve: {
     alias: {
       '@core': path.resolve(__dirname, './src/core'),
